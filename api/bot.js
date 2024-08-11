ecc7e3bc65be96763cfd5158328ed937c98a62db
Mohd Danish <63279839+mohddanish-cse@users.noreply.github.com>
2024-08-11 16:03:26 +0530

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,32 +1,14 @@
 const { Telegraf } = require('telegraf');
 
-// Retrieve the bot token from environment variables
-const token = process.env.BOT_TOKEN;
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-if (!token) {
-    console.error('BOT_TOKEN environment variable is not set.');
-    process.exit(1);
-}
+bot.start((ctx) => ctx.reply('Welcome!'));
 
-const bot = new Telegraf(token);
-
-// Handle the /start command
-bot.command('start', (ctx) => {
-    ctx.reply('Welcome! Reminders have been set up.');
+bot.command('remind', (ctx) => {
+  // Handle reminder command
+  ctx.reply('Reminder set!');
 });
 
-// Handle messages from channels
-bot.on('channel_post', (ctx) => {
-    ctx.reply('Received a post in the channel.');
-});
-
-// Start the bot
 bot.launch();
 
-console.log('Bot is up and running.');
-
-// Export the function for Vercel
-module.exports = async (req, res) => {
-    bot.handleUpdate(req.body); // Process incoming updates
-    res.status(200).send('OK');
-};
+module.exports = bot;
