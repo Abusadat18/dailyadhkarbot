2c7e9fbf3c540afb89228c417e8f62e55c99e623
Mohd Danish <63279839+mohddanish-cse@users.noreply.github.com>
2024-08-11 16:06:26 +0530

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,13 +1,18 @@
 const { Telegraf } = require('telegraf');
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Welcome!'));
+bot.start((ctx) => ctx.reply('Welcome! Your bot is up and running.'));
+bot.command('remind', (ctx) => ctx.reply('Reminder set!'));
 
-bot.command('remind', (ctx) => {
-  // Handle reminder command
-  ctx.reply('Reminder set!');
-});
+// This function will set the webhook to your Vercel endpoint
+bot.telegram.setWebhook(`${process.env.APP_URL}/api/bot`);
+bot.startPolling();
+
+// Handle webhook updates
+bot.webhookCallback = (req, res) => {
+  bot.handleUpdate(req.body);
+  res.sendStatus(200);
+};
 
 bot.launch();
 
